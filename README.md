4e6ddc662f230322593cea5d7810f3474b9a6f1c
Luca Mussi <ing.luca.mussi@gmail.com>
2014-04-24 01:40:04 +0200

    first test with accounts-users-bootstrap

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 [![Build Status](https://travis-ci.org/splendido/accounts-users-core.svg?branch=master)](https://travis-ci.org/splendido/accounts-users-core)
-
 Accounts-Users-Core
 ===================
 
--- package.js
+++ package.js
@@ -1,16 +1,23 @@
 Package.describe({
-  summary: "Users' Accounts Systems to be possibly styled for different front-end frameworks. Core package."
+    summary: "Meteor sign up and sign in templates styled for twitter Bootstrap"
 });
 
 Package.on_use(function(api, where) {
-  api.use(['check', 'deps'], ['client', 'server']);
-  api.add_files(['accounts-users-core.js'], ['client', 'server']);
-  if (api.export)
+    api.use(['check', 'deps'], ['client', 'server']);
+    api.add_files([
+        'lib/accounts-users-core.js'
+    ], ['client', 'server']);
+    /*
+    api.add_files([
+        'lib/accounts-users-core-events.js',
+        'lib/accounts-users-core-helpers.js'
+    ], ['client']);
+    */
     api.export('AccountsUsers', ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-  api.use('accounts-users-core');
-  api.use(['tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('tests/accounts-users-core_tests.js', ['client', 'server']);
+    api.use('accounts-users-core');
+    api.use(['tinytest', 'test-helpers'], ['client', 'server']);
+    api.add_files('tests/accounts-users-core_tests.js', ['client', 'server']);
 });
