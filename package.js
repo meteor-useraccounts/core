b9188d5619a2bfa24813b03a71d286705539ef86
Luca Mussi <ing.luca.mussi@gmail.com>
2014-05-29 21:57:48 +0200

    added accounts-meld as a submodule

--- package.js
+++ package.js
@@ -8,7 +8,6 @@ Package.on_use(function(api, where) {
         'check',
         'deps',
         'accounts-base',
-        'accounts-merge',
         'accounts-t9n',
         'iron-router',
     ], ['client', 'server']);
@@ -27,7 +26,7 @@ Package.on_use(function(api, where) {
 
     api.imply([
         'accounts-base',
-        'accounts-merge',
+        'accounts-meld',
         'accounts-t9n',
         'iron-router',
     ], ['client', 'server']);
