d746afa33271d2c152ec48aaf075480ce3e253d0
Luca Mussi <ing.luca.mussi@gmail.com>
2014-08-27 08:08:37 +0200

    new files for Meteor 0.9.0

--- package_0.9.0.js
+++ package_0.9.0.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor sign up and sign in templates core package.",
-    version: "0.0.21",
+    version: "0.0.20",
     name: "splendido:accounts-templates-core",
     git: "https://github.com/splendido/accounts-templates-core.git",
 });
