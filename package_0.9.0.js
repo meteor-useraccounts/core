13ad75b2a316a0d2270780760377ffb9d8d3105a
Luca Mussi <ing.luca.mussi@gmail.com>
2014-08-28 11:40:23 +0200

    Bump to version 0.0.22

--- package_0.9.0.js
+++ package_0.9.0.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Meteor sign up and sign in templates core package.",
-    version: "0.0.20",
+    version: "0.0.22",
     name: "splendido:accounts-templates-core",
     git: "https://github.com/splendido/accounts-templates-core.git",
 });
