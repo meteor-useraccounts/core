002a79b15ec214ca5a1a1cbbf1d3dd13790ebb0c
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-04 02:40:40 +0200

    Bump to version 0.0.24

--- package.js
+++ package.js
@@ -1,15 +1,42 @@
 Package.describe({
-    summary: "Meteor sign up and sign in templates core package."
+    summary: "Meteor sign up and sign in templates core package.",
+    version: "0.0.23",
+    name: "splendido:accounts-templates-core",
+    git: "https://github.com/splendido/accounts-templates-core.git",
 });
 
-Package.on_use(function(api, where) {
+Package.on_use(function(api) {
+    if (api.versionsFrom) {
+        api.versionsFrom("METEOR@0.9.0");
+
+        api.use([
+            'mrt:accounts-t9n@0.0.13',
+            'iron:router@0.9.1',
+        ], ['client', 'server']);
+
+        api.imply([
+            'accounts-base',
+            'mrt:accounts-t9n@0.0.13',
+            'iron:router@0.9.1',
+        ], ['client', 'server']);
+    }
+    else{
+        api.use([
+            'accounts-t9n',
+            'iron-router',
+        ], ['client', 'server']);
+
+        api.imply([
+            'accounts-base',
+            'accounts-t9n',
+            'iron-router',
+        ], ['client', 'server']);
+    }
 
     api.use([
         'check',
         'deps',
         'accounts-base',
-        'accounts-t9n',
-        'iron-router',
         'underscore',
     ], ['client', 'server']);
 
@@ -36,24 +63,29 @@ Package.on_use(function(api, where) {
         'lib/methods.js',
     ], ['client']);
 
-    api.imply([
-        'accounts-base',
-        'accounts-t9n',
-        'iron-router',
-    ], ['client', 'server']);
-
     api.export([
         'AccountsTemplates',
     ], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-    api.use('accounts-templates-core');
+    if (api.versionsFrom) {
+        api.use('splendido:accounts-templates-core');
+        api.use([
+            'mrt:accounts-t9n@0.0.13',
+            'iron:router@0.9.1',
+        ], ['client', 'server']);
+    }
+    else{
+        api.use([
+            'accounts-t9n',
+            'iron-router',
+        ], ['client', 'server']);
+
+    }
     api.use([
         'tinytest',
         'test-helpers',
-        'accounts-t9n',
-        'iron-router',
     ], ['client', 'server']);
     api.add_files([
         'tests/accounts-templates-core_tests.js',
