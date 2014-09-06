edce5d0bc596922ab3ef5788650fce96b596cf6a
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-06 09:45:40 +0200

    dependencies update

--- package.js
+++ package.js
@@ -10,13 +10,13 @@ Package.on_use(function(api) {
         api.versionsFrom("METEOR@0.9.0");
 
         api.use([
-            'softwarerero:accounts-t9n@0.0.13',
+            'softwarerero:accounts-t9n@0.0.17',
             'iron:router@0.9.1',
         ], ['client', 'server']);
 
         api.imply([
             'accounts-base',
-            'softwarerero:accounts-t9n@0.0.13',
+            'softwarerero:accounts-t9n@0.0.17',
             'iron:router@0.9.1',
         ], ['client', 'server']);
     }
@@ -72,7 +72,7 @@ Package.on_test(function(api) {
     if (api.versionsFrom) {
         api.use('splendido:accounts-templates-core');
         api.use([
-            'softwarerero:accounts-t9n@0.0.13',
+            'softwarerero:accounts-t9n@0.0.17',
             'iron:router@0.9.1',
         ], ['client', 'server']);
     }
