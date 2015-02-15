69d680d7ec2d44dddae2f67222aef38a75ec8790
Luca Mussi <ing.luca.mussi@gmail.com>
2015-02-15 21:14:55 +0100

    empty structure

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2014 [@splendido](https://github.com/splendido)
+Copyright (c) 2015 [UserAccounts](https://github.com/meteor-useraccounts)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
--- package.js
+++ package.js
@@ -1,86 +1,36 @@
+// Package metadata for Meteor.js web platform (https://www.meteor.com/)
+// This file is defined within the Meteor documentation at
+//
+//   http://docs.meteor.com/#/full/packagejs
+//
+// and it is needed to define a Meteor package
+
+var Both = ['client', 'server'];
+var Client = 'client';
+var Server = 'server';
+
+
 Package.describe({
-    summary: "Meteor sign up and sign in templates core package.",
-    version: "1.7.0",
-    name: "useraccounts:core",
-    git: "https://github.com/meteor-useraccounts/core.git",
+	name: 'useraccounts:core',
+	summary: 'Amazingly customizable users accounting templates (and much more!)',
+	version: '2.0.0',
+	git: 'https://github.com/meteor-useraccounts/core.git'
 });
 
-Package.on_use(function(api) {
-    api.versionsFrom("METEOR@1.0.3");
+Package.onUse(function(api) {
+	api.versionsFrom('1.0');
 
-    api.use([
-        "accounts-base",
-        "check",
-        "underscore",
-        "iron:router",
-        "reactive-var",
-    ], ["client", "server"]);
+	// Logger
+	api.use([
+		'jag:pince@0.0.5',
+	], Both);
 
-    api.use([
-        "blaze",
-        "reactive-dict",
-    ], "client");
+	// Base Class definition
+	api.addFiles([
+		'src/main.js'
+	], Both);
 
-    api.use([
-        "http"
-    ], "server");
-
-    api.imply([
-        "accounts-base",
-        "softwarerero:accounts-t9n@1.0.6",
-        "iron:router@1.0.7",
-    ], ["client", "server"]);
-
-    api.imply([
-        "templating",
-    ], ["client"]);
-
-    api.add_files([
-        "lib/field.js",
-        "lib/core.js",
-        "lib/server.js",
-        "lib/methods.js",
-    ], ["server"]);
-
-    api.add_files([
-        "lib/utils.js",
-        "lib/field.js",
-        "lib/core.js",
-        "lib/client.js",
-        "lib/templates_helpers/at_error.js",
-        "lib/templates_helpers/at_form.js",
-        "lib/templates_helpers/at_input.js",
-        "lib/templates_helpers/at_nav_button.js",
-        "lib/templates_helpers/at_oauth.js",
-        "lib/templates_helpers/at_pwd_form.js",
-        "lib/templates_helpers/at_pwd_form_btn.js",
-        "lib/templates_helpers/at_pwd_link.js",
-        "lib/templates_helpers/at_reCaptcha.js",
-        "lib/templates_helpers/at_result.js",
-        "lib/templates_helpers/at_sep.js",
-        "lib/templates_helpers/at_signin_link.js",
-        "lib/templates_helpers/at_signup_link.js",
-        "lib/templates_helpers/at_social.js",
-        "lib/templates_helpers/at_terms_link.js",
-        "lib/templates_helpers/at_title.js",
-        "lib/methods.js",
-    ], ["client"]);
-
-    api.export([
-        "AccountsTemplates",
-    ], ["client", "server"]);
-});
-
-Package.on_test(function(api) {
-    api.use("useraccounts:core@1.7.0");
-
-    api.use([
-        'accounts-password',
-        'tinytest',
-        'test-helpers'
-    ], ['client', 'server']);
-
-    api.add_files([
-        "tests/tests.js",
-    ], ["client", "server"]);
+	api.export([
+		'UA',
+	], Both);
 });
