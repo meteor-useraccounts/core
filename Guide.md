3862dd2ffee16902c9c94cc015cc0bbea3546c34
Luca Mussi <ing.luca.mussi@gmail.com>
2015-04-18 12:24:45 +0200

    fixed #342

--- Guide.md
+++ Guide.md
@@ -649,9 +649,14 @@ In case you wish to change the text for errors appearing inside the error box, y
 AccountsTemplates.configure({
     texts: {
         errors: {
+            accountsCreationDisabled: "Client side accounts creation is disabled!!!",
+            cannotRemoveService: "Cannot remove the only active service!",
+            captchaVerification: "Captcha verification failed!",
             loginForbidden: "error.accounts.Login forbidden",
             mustBeLoggedIn: "error.accounts.Must be logged in",
             pwdMismatch: "error.pwdsDontMatch",
+            validationErrors: "Validation Errors",
+            verifyEmailFirst: "Please verify your email first. Check the email and follow the link!",
         }
     }
 });
--- package.js
+++ package.js
@@ -1,88 +1,89 @@
 Package.describe({
-    summary: "Meteor sign up and sign in templates core package.",
-    version: "1.9.0",
-    name: "useraccounts:core",
-    git: "https://github.com/meteor-useraccounts/core.git",
+  summary: 'Meteor sign up and sign in templates core package.',
+  version: '1.8.1',
+  name: 'useraccounts:core',
+  git: 'https://github.com/meteor-useraccounts/core.git',
 });
 
+
 Package.on_use(function(api) {
-    api.versionsFrom("METEOR@1.0.3");
+  api.versionsFrom('METEOR@1.0.3');
 
-    api.use([
-        "accounts-base",
-        "check",
-        "underscore",
-        "iron:router",
-        "reactive-var",
-    ], ["client", "server"]);
+  api.use([
+    'accounts-base',
+    'check',
+    'underscore',
+    'iron:router',
+    'reactive-var',
+  ], ['client', 'server']);
 
-    api.use([
-        "blaze",
-        "reactive-dict",
-    ], "client");
+  api.use([
+    'blaze',
+    'reactive-dict',
+  ], 'client');
 
-    api.use([
-        "http"
-    ], "server");
+  api.use([
+    'http'
+  ], 'server');
 
-    api.imply([
-        "accounts-base",
-        "softwarerero:accounts-t9n@1.0.6",
-        "iron:router@1.0.7",
-    ], ["client", "server"]);
+  api.imply([
+    'accounts-base',
+    'softwarerero:accounts-t9n@1.0.6',
+    'iron:router@1.0.7',
+  ], ['client', 'server']);
 
-    api.imply([
-        "templating",
-    ], ["client"]);
+  api.imply([
+    'templating',
+  ], ['client']);
 
-    api.add_files([
-        "lib/field.js",
-        "lib/core.js",
-        "lib/server.js",
-        "lib/methods.js",
-    ], ["server"]);
+  api.add_files([
+    'lib/field.js',
+    'lib/core.js',
+    'lib/server.js',
+    'lib/methods.js',
+  ], ['server']);
 
-    api.add_files([
-        "lib/utils.js",
-        "lib/field.js",
-        "lib/core.js",
-        "lib/client.js",
-        "lib/templates_helpers/at_error.js",
-        "lib/templates_helpers/at_form.js",
-        "lib/templates_helpers/at_input.js",
-        "lib/templates_helpers/at_nav_button.js",
-        "lib/templates_helpers/at_oauth.js",
-        "lib/templates_helpers/at_pwd_form.js",
-        "lib/templates_helpers/at_pwd_form_btn.js",
-        "lib/templates_helpers/at_pwd_link.js",
-        "lib/templates_helpers/at_reCaptcha.js",
-        "lib/templates_helpers/at_resend_verification_email_link.js",
-        "lib/templates_helpers/at_result.js",
-        "lib/templates_helpers/at_sep.js",
-        "lib/templates_helpers/at_signin_link.js",
-        "lib/templates_helpers/at_signup_link.js",
-        "lib/templates_helpers/at_social.js",
-        "lib/templates_helpers/at_terms_link.js",
-        "lib/templates_helpers/at_title.js",
-        "lib/templates_helpers/at_message.js",
-        "lib/methods.js",
-    ], ["client"]);
+  api.add_files([
+    'lib/utils.js',
+    'lib/field.js',
+    'lib/core.js',
+    'lib/client.js',
+    'lib/templates_helpers/at_error.js',
+    'lib/templates_helpers/at_form.js',
+    'lib/templates_helpers/at_input.js',
+    'lib/templates_helpers/at_nav_button.js',
+    'lib/templates_helpers/at_oauth.js',
+    'lib/templates_helpers/at_pwd_form.js',
+    'lib/templates_helpers/at_pwd_form_btn.js',
+    'lib/templates_helpers/at_pwd_link.js',
+    'lib/templates_helpers/at_reCaptcha.js',
+    'lib/templates_helpers/at_resend_verification_email_link.js',
+    'lib/templates_helpers/at_result.js',
+    'lib/templates_helpers/at_sep.js',
+    'lib/templates_helpers/at_signin_link.js',
+    'lib/templates_helpers/at_signup_link.js',
+    'lib/templates_helpers/at_social.js',
+    'lib/templates_helpers/at_terms_link.js',
+    'lib/templates_helpers/at_title.js',
+    'lib/templates_helpers/at_message.js',
+    'lib/methods.js',
+  ], ['client']);
 
-    api.export([
-        "AccountsTemplates",
-    ], ["client", "server"]);
+  api.export([
+    'AccountsTemplates',
+  ], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-    api.use("useraccounts:core@1.9.0");
+  api.use('useraccounts:core@1.8.1');
 
-    api.use([
-        'accounts-password',
-        'tinytest',
-        'test-helpers'
-    ], ['client', 'server']);
+  api.use([
+    'accounts-password',
+    'tinytest',
+    'test-helpers'
+  ], ['client', 'server']);
 
-    api.add_files([
-        "tests/tests.js",
-    ], ["client", "server"]);
+  api.add_files([
+    'tests/tests.js',
+  ], ['client', 'server']);
 });
