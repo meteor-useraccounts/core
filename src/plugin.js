3f628d0ea2c0b079251396341f657caf982a88cf
Luca Mussi <ing.luca.mussi@gmail.com>
2015-10-05 22:17:14 +0200

    removed unuseful stuff

--- src/plugin.js
+++ src/plugin.js
@@ -28,47 +28,4 @@ _.extend(UAPlugin.prototype, {
    */
   _id: 'UAPlugin',
 
-  /**
-   *
-   */
-  texts: {},
-
-  /**
-   * getText - description
-   *
-   * @param  {type} key description
-   * @return {type}     description
-   */
-  getText: function getText(key) {
-    var self = this;
-    var uaTmpl = Template.currentData().instance;
-    var state = uaTmpl.getState();
-    var texts = self.texts[state] || self.texts.default;
-
-    return texts[key] || '';
-  },
-
-  /**
-   * skinClasses - description
-   *
-   * @param  {type} element description
-   * @return {type}         description
-   */
-  skinClasses: function skinClasses(element) {
-    var self = this;
-    var framework = Template.currentData().instance.framework;
-    var classes;
-
-    UALog.trace('plugin ' + this._id + ': skinClasses - ' + element);
-
-    if (_.has(self.skins, framework)) {
-      classes = self.skins[framework][element];
-      if (_.isFunction(classes)) {
-        classes = classes();
-      }
-      return classes || '';
-    }
-
-    return '';
-  },
 });
